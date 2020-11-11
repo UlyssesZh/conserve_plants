878aec39f96857deebf364237233ef3339d227b1
UlyssesZh <2938747508@qq.com>
2020-11-11 15:03:44 +0800

    Fixed min_variance.rb and added plan.txt

--- min_variance.rb
+++ min_variance.rb
@@ -3,7 +3,7 @@ require_relative 'plant'
 BEGIN_TIME = Time.now
 
 def Array.product array, *arrays
-	array.to_enum :product, *arrays
+	array.to_a.to_enum :product, *arrays.map(&:to_a)
 end
 
 TAKEN = 14796153683959
@@ -12,7 +12,7 @@ DURATION = TAKEN.max_by(&:duration).duration
 
 FACTOR = 100.0 / TAKEN.reduce(1) { _1 * (DURATION - _2.duration + 1) }
 
-Array.product(*TAKEN.map { (0..DURATION - _1.duration).to_a }).min_by.with_index do |starts, i|
+result = Array.product(*TAKEN.map { 0..DURATION - _1.duration }).min_by.with_index do |starts, i|
 	print "\r#{(i * FACTOR).round}%"
 	DURATION.times.sum do |year|
 		TAKEN_ARRAY.zip(starts).sum do |plant, start|
@@ -20,3 +20,7 @@ Array.product(*TAKEN.map { (0..DURATION - _1.duration).to_a }).min_by.with_index
 		end ** 2
 	end
 end
+
+puts "\r100%"
+p result
+puts "Time passed: #{Time.now - BEGIN_TIME}s"
